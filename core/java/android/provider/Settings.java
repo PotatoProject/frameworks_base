528aef4bde53f526fcc27c5174e3f3eb61ab2c23
Dalingrin <dalingrin@gmail.com>
2013-12-12 03:19:48 -0600

    SystemUI: Add double-tap status bar to sleep [1/2]
    Adin's edit: format to follow AOSP Java Code Style
    
    Change-Id: I4c54718e8ec8774677d13917b3a7ffa3b4b18cb5
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4520,6 +4520,11 @@ public final class Settings {
          */
         public static final String STATUS_BAR_LOGO = "status_bar_logo";
 
+        /**
+         *  Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /** Whether to pulse ambient on new music tracks
          *
