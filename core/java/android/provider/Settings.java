1a49a79226bd53908d1e8eab82ebbf7faafaa081
Danesh M <daneshm90@gmail.com>
2014-11-27 14:17:19 +0100

    BatteryMeterDrawable: add battery styles
     - Portrait
     - Landscape
     - Circle
     - Battery percent views (Inside/Ouside/Hidden)
    
    Change-Id: Icfc14d989eebde9014538291c24aa76d328df234

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3926,13 +3926,15 @@ public final class Settings {
         /**
          * Setting to determine whether or not to show the battery percentage in the status bar.
          *    0 - Don't show percentage
-         *    1 - Show percentage
+         *    1 - Show percentage outside the battery icon
+         *    2 - Show percentage inside the battery icon
          * @hide
          */
         public static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /** @hide */
-        private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR = sBooleanValidator;
+        private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR =
+                new InclusiveIntegerRangeValidator(0, 2);
 
         /**
          * Custom button brightness value for manual mode
@@ -7706,6 +7708,16 @@ public final class Settings {
          * @hide
          */
         public static final String FACE_AUTO_UNLOCK = "face_auto_unlock";
+
+        /**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 1: Display the battery an icon in landscape mode
+         * 2: Display the battery as a circle
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /**
          * This are the settings to be backed up.
