b4dbe90629b0e42674de2199e7b0868d32c506ba
Carlo Savignano <carlosavignano@aospa.co>
2016-09-30 15:56:48 -0400

    SystemUI: alert slider integration
    * Volume panel UI adjustements for alert slider devices.
    * Add quick settings tile for extra settings within detail view, disabled when zen is off.
    * Silent state: Total silence / Alarms only.
    * Priority state: Near-stock without automatic and times' rules, permanent introduction
      with customize button and exposed only events and reminders toggles).
    * Set DND tile visibility only when registering volume panel.
    * Add missing priority icon for zen footer, both w/ and w/o alert slider.
    
    TODO> Merge with AOSP Do Not Disturb tile and policy.
    
    Change-Id: I5c09199f744d0e3fe8abed32310bbb9d91e1c970
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>
    Signed-off-by: joshuous <joshuous@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -23,6 +23,7 @@ import com.android.systemui.plugins.qs.*;
 import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
+import com.android.systemui.qs.tiles.AlertSliderTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
@@ -70,6 +71,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("saver")) return new DataSaverTile(mHost);
         else if (tileSpec.equals("night")) return new NightDisplayTile(mHost);
         else if (tileSpec.equals("nfc")) return new NfcTile(mHost);
+        else if (tileSpec.equals("alertslider")) return new AlertSliderTile(mHost);
         else if (tileSpec.equals("sound")) return new SoundTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
