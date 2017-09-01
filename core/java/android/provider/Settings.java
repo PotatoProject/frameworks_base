10ed4ca3fee78004e3bc71da9b8f8f69624cc5ff
ezio84 <brabus84@gmail.com>
2017-09-01 15:28:27 +0200

    base: Double tap to sleep on lockscreen [1/2]
    Change-Id: Id8638b0716abf8b264ea9dc2b8dceb892e019aa0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5204,6 +5204,13 @@ public final class Settings {
         public static final String NAVIGATION_HANDLE_WIDTH = "navigation_handle_width";
 
         /**
+         * Double tap on lockscreen to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN =
+                "double_tap_sleep_lockscreen";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5340,6 +5347,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE_VENDOR_HINT);
             PRIVATE_SETTINGS.add(NAVIGATION_HANDLE_WIDTH);
+            PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
         }
 
         /**
