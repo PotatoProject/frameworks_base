6727a098c85480cde59984c0dcc510e5639139ec
ezio84 <brabus84@gmail.com>
2018-01-04 10:26:17 +0100

    Allow to suppress notifications sound/vibration if screen is ON [1/2]
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>
    Change-Id: I3b0761ade77507341a22780738496fb3e222e562

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4632,6 +4632,14 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Wheter to play notification sound and vibration if screen is ON
+         * 0 - never
+         * 1 - always
+         * @hide
+         */
+        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
