2b10ba5c02001be1e35732b26d11b8f7981ee209
Alex Cruz <du.alexcruz@gmail.com>
2018-01-10 16:36:10 -0500

    Hide lockscreen items [1/2]
    This adds the ability to add items such as data, clock and alarm in the
    lockscreen.
    
    Thanks to beanstown106 for the tip on the alarm text mod
    
    Change-Id: I42c7ad0c3de0b41a653b9b687acfe9536bf80a44

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4020,6 +4020,26 @@ public final class Settings {
          */
         public static final String POWERMENU_AIRPLANE = "powermenu_airplane";
 
+		/**
+		 * Hide lockscreen date
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_DATE = "hide_lockscreen_date";
+
+        /**
+         * Hide lockscreen clock
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_CLOCK = "hide_lockscreen_clock";
+
+        /**
+         * Hide lockscreen alarm
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_ALARM = "hide_lockscreen_alarm";
+
+        /**
+         * Change volume up and down handlign based on rotation
         /**
          * Whether to mute annoying notifications
          * @hide
