367fedb516cabcf323036ef0fe1926a2d3575778
Nico60 <ndhouailly@gmail.com>
2018-03-18 16:53:35 +0100

    Quick settings panel : Set transparency [1/2]
    Change-Id: I309eeb1bfca6dc7db1bacd33c86199f422aa87b0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3994,6 +3994,12 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
 
         /**
+         * Whether to change the transparency of the qs panel
+         * @hide
+         */
+        public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
