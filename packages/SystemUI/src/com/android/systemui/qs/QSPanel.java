23ebfd21e1fafa1e42844e7d9772ca2946f58bfb
maxwen <max.weninger@gmail.com>
2018-04-03 18:10:05 +0200

    base: SystemUI: add tuner pref for qs brightness slider position
    Change-Id: I25a8cbc0a95be06baf5f5c3a54d877f801fb886e
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -64,6 +64,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     private static final String TAG = "QSPanel";
 
     public static final String QS_SHOW_BRIGHTNESS = "qs_show_brightness";
+    public static final String QS_BRIGHTNESS_POSITION_BOTTOM = "qs_brightness_position_bottom";
 
     protected final Context mContext;
     protected final ArrayList<TileRecord> mRecords = new ArrayList<TileRecord>();
@@ -230,6 +231,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         Dependency.get(TunerService.class).addTunable(this, QS_SHOW_BRIGHTNESS);
+        Dependency.get(TunerService.class).addTunable(this, QS_BRIGHTNESS_POSITION_BOTTOM);
         if (mHost != null) {
             setTiles(mHost.getTiles());
         }
@@ -268,6 +270,25 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         } catch (Exception e){
             Log.d(TAG, "Caught exception from Tuner", e);
         }
+        if (QS_BRIGHTNESS_POSITION_BOTTOM.equals(key)) {
+            if (newValue == null || Integer.parseInt(newValue) == 0) {
+                removeView(mBrightnessView);
+                addView(mBrightnessView, 0);
+            } else {
+                removeView(mBrightnessView);
+                addView(mBrightnessView, getBrightnessViewPositionBottom());
+            }
+        }
+    }
+
+    private int getBrightnessViewPositionBottom() {
+        for (int i = 0; i < getChildCount(); i++) {
+            View v = getChildAt(i);
+            if (v == mPageIndicator) {
+                return i;
+            }
+        }
+        return 0;
     }
 
     public void openDetails(String subPanel) {
