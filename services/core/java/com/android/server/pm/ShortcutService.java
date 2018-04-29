380b269008c7f8025a5d90bdbc4ac705cab48c40
ezio84 <brabus84@gmail.com>
2018-04-29 21:33:24 +0200

    Always refresh system apps launcher shortcuts on build date change
    e.g. Settings apps shortcuts icons being derped after a dirty flash
    before this commit, that was fixed after changing the system language
    
    this follows maxwen 's package cache cleanup commits chain
    thanks Myself5 for the ro.build.date idea
    
    [TheCrazyLex: Adjusted to use our Build.DATE field.]
    
    Change-Id: Id27c706385ff75de1b6ec64feaf73e80d77a8d8e
    Signed-off-by: Alex Naidis <alex.naidis@paranoidandroid.co>
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>

--- services/core/java/com/android/server/pm/ShortcutService.java
+++ services/core/java/com/android/server/pm/ShortcutService.java
@@ -3924,7 +3924,7 @@ public class ShortcutService extends IShortcutService.Stub {
     // Injection point.
     @VisibleForTesting
     String injectBuildFingerprint() {
-        return Build.FINGERPRINT;
+        return Build.DATE;
     }
 
     final void wtf(String message) {
