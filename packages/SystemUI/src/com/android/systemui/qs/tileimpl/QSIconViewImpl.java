ddd6ccfeeb92f4dbb40d9e9ee1c43db2f1a27c3e
Adarsh-MR <adarshmr1998@gmail.com>
2018-05-27 08:04:03 +0000

    Themes: Expose QS tiles and label colors
    * also exposed expand indicator and underline colors
    
    * also disabling grayscale tint if config_enable_qs_tile_tinting enabled.
      because switching tiles state (enable/disable) will apply grayscale tint and removes
      themed tint color,  big thanks to Gurpreet ( SNN Infinity) for headsup on this.
      this bool also prevents black on black issue if a themer doesn't change tiles tint colors.
    
    * forward-port and adapt to Android Pie
    * Android Pie changes:
      Added resource for QS secondary label, qs tile backgrounds.
      Updated exposed colors with Android Pie changes.
    * POSP specific changes:
      Made the change work with the qs panel bg commits
    Change-Id: Iba15ad4013378a815607894a36ee18ea30bfcc3b
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
@@ -173,15 +173,19 @@ public class QSIconViewImpl extends QSIconView {
             final float fromChannel = Color.red(fromColor);
             final float toChannel = Color.red(toColor);
 
+            boolean enableQsTileTinting = getContext().getResources().getBoolean(R.bool.config_enable_qs_tile_tinting);
+
             ValueAnimator anim = ValueAnimator.ofFloat(0, 1);
             anim.setDuration(QS_ANIM_LENGTH);
             anim.addUpdateListener(animation -> {
                 float fraction = animation.getAnimatedFraction();
                 int alpha = (int) (fromAlpha + (toAlpha - fromAlpha) * fraction);
                 int channel = (int) (fromChannel + (toChannel - fromChannel) * fraction);
+
                 boolean setQsFromResources = System.getIntForUser(getContext().getContentResolver(),
                             System.QS_PANEL_BG_USE_FW, 1, UserHandle.USER_CURRENT) == 1;
-                if (setQsFromResources) {
+
+                if (setQsFromResources && !enableQsTileTinting) {
                     setTint(iv, Color.argb(alpha, channel, channel, channel));
                 } else {
                     setTint(iv, toColor);
