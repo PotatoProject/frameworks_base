08bb6f4918dc02d13f9127c727d82af2a4e7c1fc
e.min <e.min@samsung.com>
2018-05-28 13:31:42 +0900

    fix synchronization bug when notification enqueue/cancel
    Test: this is a bug fix,
    Test: some app do startForegroundService() and then stopForegroundService() right away.
    Test: at this time, enqueue runnable and cancel runnable are conflicted.
    Test: EnqueueNotificationRunnable -> CancelNotification -> PostNotificationRunnable
    Test: usually, in this moment 3rd notification posting is not executed.
    Test: but, if app want to keep their notification, handler queue order is like below.
    Test: EnqueueNotificationRunnable -> removeForegroundServiceFlagFromNotification() -> CancelNotification -> PostNotificationRunnable.
    Test: 4th runnable would be canceled. but in removeForegroundServiceFlagFromNotification(),
    Test: notification is posted to all listener and synchronization is broken.
    Test: NMS doesn't have a notification and all listener module has a ghost notification.
    
    Change-Id: I1f9a4cdb5c3eb8174286fda6ca54b0f1793702ce
    Signed-off-by: Adesh15 <adesikha15@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -3439,9 +3439,9 @@ public class NotificationManagerService extends SystemService {
                 public void run() {
                     synchronized (mNotificationLock) {
                         removeForegroundServiceFlagByListLocked(
-                                mEnqueuedNotifications, pkg, notificationId, userId);
+                                mEnqueuedNotifications, pkg, notificationId, userId, false);
                         removeForegroundServiceFlagByListLocked(
-                                mNotificationList, pkg, notificationId, userId);
+                                mNotificationList, pkg, notificationId, userId, true);
                     }
                 }
             });
@@ -3450,7 +3450,7 @@ public class NotificationManagerService extends SystemService {
         @GuardedBy("mNotificationLock")
         private void removeForegroundServiceFlagByListLocked(
                 ArrayList<NotificationRecord> notificationList, String pkg, int notificationId,
-                int userId) {
+                int userId, boolean needPosted) {
             NotificationRecord r = findNotificationByListLocked(
                     notificationList, pkg, null, notificationId, userId);
             if (r == null) {
@@ -3463,8 +3463,10 @@ public class NotificationManagerService extends SystemService {
             // initially *and* force remove FLAG_FOREGROUND_SERVICE.
             sbn.getNotification().flags =
                     (r.mOriginalFlags & ~Notification.FLAG_FOREGROUND_SERVICE);
-            mRankingHelper.sort(mNotificationList);
-            mListeners.notifyPostedLocked(sbn, sbn /* oldSbn */);
+            if (needPosted) {
+                mRankingHelper.sort(mNotificationList);
+                mListeners.notifyPostedLocked(sbn, sbn /* oldSbn */);
+            }
         }
     };
 
