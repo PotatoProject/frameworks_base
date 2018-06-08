e7e8a45310ae2828b8b39b409c40f08e974b67e7
Kshitij Gupta <kshitijgm@gmail.com>
2018-06-08 15:50:44 +0000

    StatusBar: Add missing observer for QS-Panel wallpaper-color setting
    - Missed this change implementing it.
    - Necessary for updating QS colors immediately.
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: I9761701d4edba5b370bee982e665dc2d34b6d174

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4878,6 +4878,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.SYSUI_ROUNDED_FWVALS),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_BG_USE_WALL),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
