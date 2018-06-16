4ee20b77b1c6a611b95dde8ce36b9f64047720da
Kshitij Gupta <kshitijgm@gmail.com>
2018-06-17 01:54:49 +0530

    QS: Separate out wallpaper and user color values
    - Move out the wallpaper colors to another setting and keep the users
      color value exclusive.
    - Set colors on QS based on the Wallpaper colors toggle.
    - This allows us to technically "restore" the old color when the
      wallpaper preference is toggled off.
    
    Change-Id: Ib3ec52cf0dcad2acf338e8198fb8be535b021769
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4085,16 +4085,22 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
 
         /**
-         * Whether to change the transparency of the qs panel
+         * Transparency of the qs panel
          * @hide
          */
         public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
 
         /**
-         * Whether to change the color of the qs panel
+         * Color of the qs panel
          * @hide
          */
         public static final String QS_PANEL_BG_COLOR = "qs_panel_bg_color";
+
+        /**
+         * Color of the qs panel evaluated from wallpaper
+         * @hide
+         */
+        public static final String QS_PANEL_BG_COLOR_WALL = "qs_panel_bg_color_wall";
 
         /**
          * Whether to change the color of the qs panel based on wallpaper
