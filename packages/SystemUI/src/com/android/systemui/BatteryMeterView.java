cbd956a1a2287edb0dd58a6f8a70197c9e15a436
Adrian DC <radian.dc@gmail.com>
2018-06-17 12:38:23 +0200

    Resolve status bar battery percentage tints
     * Use the tint color matching the status bar texts
    
     * Results: Before and after: https://ibb.co/gvGm7y
    
     * Test: Bright wallpaper with status bar contrasts:
        https://ibb.co/dxupSy
    
    Change-Id: I8aa619dc3ae9e5daa6390b2d91f918792018f69b

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -283,7 +283,7 @@ public class BatteryMeterView extends LinearLayout implements
         int background = getColorForDarkIntensity(intensity, mLightModeBackgroundColor,
                 mDarkModeBackgroundColor);
         mDrawable.setColors(foreground, background);
-        setTextColor(foreground);
+        setTextColor(tint);
     }
 
     public void setTextColor(int color) {
