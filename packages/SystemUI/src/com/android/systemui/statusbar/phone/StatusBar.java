2a90b64c3cfaf13559e5ee26a25c9bc7c0590fa4
Kshitij Gupta <kshitijgm@gmail.com>
2018-06-17 15:08:10 +0530

    wedges: Make activity indicators toggleable (2/2)
    Change-Id: I6531190d0a71d7a7f76c98520ed0490479c43d55
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -866,6 +866,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_PANEL_BG_USE_WALL),
                     false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.ACTIVITY_INDICATORS),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -881,6 +884,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.SHOW_BATTERY_PERCENT)) ||
                     uri.equals(Settings.Secure.getUriFor(Settings.Secure.STATUS_BAR_BATTERY_STYLE))) {
                 setStatusBarOptions();
+            } else if (uri.equals(Settings.System.getUriFor(Settings.System.ACTIVITY_INDICATORS))) {
+                try {
+                    inflateSignalClusters();
+                } catch (Exception e) {
+                    Log.e(TAG, e);
+                }
             }
         }
 
