26ad3493956fb25386e9454191c5b6dc402a6300
xyyx <xyyx@mail.ru>
2018-09-10 08:46:56 +0800

    HWKeys: Fix annoying trouble with wakeup from hw keys
    Change-Id: Id1b137e1d73fc8d39cb7b76f312637dc699e4b9e
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6134,8 +6134,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     + " policyFlags=" + Integer.toHexString(policyFlags));
         }
 
-        // Disable hw keys in Ambient
-        if (isDozeMode() && (appSwitchKey || homeKey || menuKey || backKey)) {
+        // Disable hw keys in Ambient and when screen off
+        if ((isDozeMode() || !isScreenOn()) && (appSwitchKey || homeKey || menuKey || backKey)) {
             return 0;
         }
 
