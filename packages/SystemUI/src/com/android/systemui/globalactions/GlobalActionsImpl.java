af52fe7cb3dae40dcd2b03182ed1f1bc161efdb8
rajadeja <raj.jdja@gmail.com>
2018-12-12 08:06:23 +0100

    Revert "Powermenu: use new gradient dialog also for"
    This reverts commit af8115208abc9f508707581e342147da9e2542ce.
    
    Change-Id: Iadc6024f47a4e6d6524792a2bafc2092dc65b818

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
@@ -23,7 +23,6 @@ import android.app.WallpaperManager;
 import android.content.Context;
 import android.graphics.Point;
 import android.view.View;
-import android.os.PowerManager;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
@@ -115,14 +114,7 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
         bar.getIndeterminateDrawable().setTint(color);
         TextView message = d.findViewById(R.id.text1);
         message.setTextColor(color);
-
-        if (isReboot && PowerManager.REBOOT_BOOTLOADER.equals(reason)) {
-            message.setText(com.android.internal.R.string.reboot_to_bootloader_message);
-        } else if (isReboot && PowerManager.REBOOT_RECOVERY.equals(reason)) {
-            message.setText(com.android.internal.R.string.reboot_to_recovery_message);
-        } else if (isReboot) {
-            message.setText(R.string.reboot_message);
-        }
+        if (isReboot) message.setText(R.string.reboot_to_reset_message);
 
         Point displaySize = new Point();
         mContext.getDisplay().getRealSize(displaySize);
