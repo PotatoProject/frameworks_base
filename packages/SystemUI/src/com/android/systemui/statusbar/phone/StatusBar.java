e155f98480e5ddcdb25185f37e38d4c9b17dfb9f
Kshitij Gupta <kshitijgm@gmail.com>
2018-12-21 20:50:49 +0000

    base: qs: Apply bg to active qs icons
    Change-Id: Ib22d5d490fb4856ab4cdf0a033c4373374529214
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1776,6 +1776,15 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_PANEL_BG_USE_WALL),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_BG_USE_FW),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                     Settings.System.QS_PANEL_BG_COLOR),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_BG_COLOR_WALL),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -1788,6 +1797,11 @@ public class StatusBar extends SystemUI implements DemoMode,
                 setPulseOnNewTracks();
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_BG_USE_WALL))) {
                 updateQSPanel();
+                mQSPanel.getHost().reloadAllTiles();
+            } else if (uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_BG_USE_FW)) ||
+                    uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_BG_COLOR)) ||
+                    uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_BG_COLOR_WALL))) {
+                mQSPanel.getHost().reloadAllTiles();
             }
         }
 
