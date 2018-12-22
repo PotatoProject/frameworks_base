87a4a4800afc4ac30930bd5a0a799774e87cbacb
Ali B <abittin@gmail.com>
2018-12-22 22:49:32 +0300

    base: SystemUI: add keyguard weather style (1/2)
    Choose from Omni style with dedicated line of
    location, conditionimage, temperature
    or KeygardSlice style added to the dateline with
    conditionimage, temperature. Care is taken to properly
    display in ambient display.
    
    Change-Id: I6f09967843d360a82b6c98b8e3d1c9a269af798f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4571,6 +4571,17 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+        * 0: OmniJaws Style
+        * 1: KeyguardSlice Style
+        * @hide
+        */
+        public static final String AICP_LOCKSCREEN_WEATHER_STYLE = "lockscreen_weather_style";
+
+         /** @hide */
+        private static final Validator AICP_LOCKSCREEN_WEATHER_STYLE_VALIDATOR =
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 1);;
+
+         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4641,6 +4652,7 @@ public final class Settings {
             CUSTOM_DEVICE_FEATURE_SETTINGS,
             CUSTOM_BACK_GESTURE_HEIGHT,
             OMNI_LOCKSCREEN_WEATHER_ENABLED,
+            AICP_LOCKSCREEN_WEATHER_STYLE,
         };
 
         /**
@@ -4767,6 +4779,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(PROXIMITY_ON_WAKE);
             PRIVATE_SETTINGS.add(CUSTOM_BACK_GESTURE_HEIGHT);
             PRIVATE_SETTINGS.add(OMNI_LOCKSCREEN_WEATHER_ENABLED);
+            PRIVATE_SETTINGS.add(AICP_LOCKSCREEN_WEATHER_STYLE);
         }
 
         /**
@@ -4868,6 +4881,7 @@ public final class Settings {
             VALIDATORS.put(CUSTOM_BACK_GESTURE_HEIGHT, CUSTOM_BACK_GESTURE_HEIGHT_VALIDATOR);
             VALIDATORS.put(OMNI_LOCKSCREEN_WEATHER_ENABLED,
                     OMNI_LOCKSCREEN_WEATHER_ENABLED_VALIDATOR);
+            VALIDATORS.put(AICP_LOCKSCREEN_WEATHER_STYLE,AICP_LOCKSCREEN_WEATHER_STYLE_VALIDATOR);
         }
 
         /**
@@ -5187,6 +5201,7 @@ public final class Settings {
          * the app must send an intent with the action {@link
          * android.provider.Settings#ACTION_MANAGE_WRITE_SETTINGS}, which causes
          * the system to display a permission management screen.
+         *
          *
          * @param context App context.
          * @return true if the calling app can write to system settings, false otherwise
