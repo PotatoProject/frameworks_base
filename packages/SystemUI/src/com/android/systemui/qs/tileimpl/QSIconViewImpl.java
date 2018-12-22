f802ec33ccba391985f44b0379f690aeb0db7e67
Kshitij Gupta <kshitijgm@gmail.com>
2018-12-22 18:25:33 +0000

    base: qs-colors: Minor fixups
    - Fixes an issue where qs tiles would be handled poorly on the default
      background. This is because the color extractor gives us null on
      default wallpaper when we try to get systemColors to update our system
      setting for QS to handle. Because of this, our QS never updates
      overlays and results in white-on-white.
    - Also fix an issue where the qs tile, when it's state is changed
      doesn't update icon color correctly. Fixed by allowing colors to be used
      in method animateGrayScale, which is designed to convert colors to gray
      scale and then animate change during toggle. This breaks our color
      leaving us with grey (and often even white/black) icons.
    
    Change-Id: I2e7d214b63414fca1cb189bf2d9366bfa2bbedf7
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
@@ -26,6 +26,8 @@ import android.graphics.Color;
 import android.graphics.drawable.Animatable2;
 import android.graphics.drawable.Animatable2.AnimationCallback;
 import android.graphics.drawable.Drawable;
+import android.os.UserHandle;
+import android.provider.Settings.System;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
@@ -177,8 +179,13 @@ public class QSIconViewImpl extends QSIconView {
                 float fraction = animation.getAnimatedFraction();
                 int alpha = (int) (fromAlpha + (toAlpha - fromAlpha) * fraction);
                 int channel = (int) (fromChannel + (toChannel - fromChannel) * fraction);
-
-                setTint(iv, Color.argb(alpha, channel, channel, channel));
+                boolean setQsFromResources = System.getIntForUser(getContext().getContentResolver(),
+                            System.QS_PANEL_BG_USE_FW, 1, UserHandle.USER_CURRENT) == 1;
+                if (setQsFromResources) {
+                    setTint(iv, Color.argb(alpha, channel, channel, channel));
+                } else {
+                    setTint(iv, toColor);
+                }
             });
             anim.addListener(new AnimatorListenerAdapter() {
                 @Override
