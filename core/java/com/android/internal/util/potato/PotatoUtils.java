6a5f7698544208a9b53e135f7518ffe91b4742d7
Alex Cruz <du.alexcruz@gmail.com>
2019-01-27 20:39:45 +0530

    Utils: Add method to determine battery temp
    Change-Id: Ifa1c2d5f4320ad6cc12452fa7bf4dfd08e1cb2f4

--- core/java/com/android/internal/util/potato/PotatoUtils.java
+++ core/java/com/android/internal/util/potato/PotatoUtils.java
@@ -18,8 +18,10 @@ package com.android.internal.util.potato;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.hardware.input.InputManager;
+import android.os.BatteryManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
@@ -115,4 +117,17 @@ public class PotatoUtils {
             }
         }
     }
+
+	public static String batteryTemperature(Context context, Boolean ForC) {
+		Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+		float temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+		return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
+	}
 }
