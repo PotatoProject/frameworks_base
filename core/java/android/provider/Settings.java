7f4312105068164085939b4d133e5417e9c3814c
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-03-01 10:03:32 +0100

    NetworkTraffic: Individual toggles for QS and statusbar indicators [1/2]
    Change-Id: I6d7f6cb27d13d5c532d43124d016e04532d8138a
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4576,6 +4576,12 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
+	/**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE_SB = "network_traffic_state_sb";
+
         /**
          * Network traffic inactivity threshold (default is 1 kBs)
          * @hide
