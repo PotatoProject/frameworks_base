cf8ca6d4d0ec920aed0e63b6cd29f26321e55872
Kshitij Gupta <kshitijgm@gmail.com>
2019-03-31 04:37:38 +0530

    base: Fix icon tint on dark themes [1/2]
    - We're now managing colors of settings icon for tint and dark/black
      states with different packages altogether. Should correctly toggle
      based on user settings for all.
    
    Change-Id: If73451d000a03f6d15e2e8216f30e867b2a16ee7
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2354,10 +2354,12 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     }
 
-    private void setIconTintOverlay() {
+    private void setIconTintOverlay(boolean isDark) {
         final boolean enable = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.SETTINGS_ICON_TINT, 0, UserHandle.USER_CURRENT) == 1;
         try {
+            mOverlayManager.setEnabled("com.potato.overlay.settingsicontint_dark",
+                    isDark && !enable, mLockscreenUserManager.getCurrentUserId());
             mOverlayManager.setEnabled("com.potato.overlay.settingsicontint",
                         enable, mLockscreenUserManager.getCurrentUserId());
         } catch (RemoteException e) {
@@ -4213,7 +4215,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 Settings.System.PREFER_BLACK_THEMES, 0, UserHandle.USER_CURRENT) == 1);
 
         handleThemeStates(useBlackTheme, useDarkTheme);
-
         // Lock wallpaper defines the color of the majority of the views, hence we'll use it
         // to set our default theme.
         final boolean lockDarkText = mColorExtractor.getColors(WallpaperManager.FLAG_LOCK, true
@@ -4243,6 +4244,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         updateCorners();
         updateQSPanel();
+        setIconTintOverlay(useDarkTheme);
     }
 
     private void updateCorners() {
@@ -5018,7 +5020,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                 setHeadsUpStoplist();
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.HEADS_UP_BLACKLIST_VALUES))) {
                 setHeadsUpBlacklist();
-            } else if (uri.equals(Settings.System.getUriFor(Settings.System.PREFER_BLACK_THEMES))) {
+            } else if (uri.equals(Settings.System.getUriFor(Settings.System.PREFER_BLACK_THEMES)) ||
+                    uri.equals(Settings.System.getUriFor(Settings.System.SETTINGS_ICON_TINT))) {
                 updateTheme();
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.QS_ROWS_PORTRAIT)) ||
                     uri.equals(Settings.System.getUriFor(Settings.System.QS_ROWS_LANDSCAPE)) ||
@@ -5046,8 +5049,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 setForceAmbient();
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.USE_OREO_SETTINGS))) {
                 updateSettingsTiles();
-            } else if (uri.equals(Settings.System.getUriFor(Settings.System.SETTINGS_ICON_TINT))) {
-                setIconTintOverlay();
             }
         }
 
@@ -5066,7 +5067,6 @@ public class StatusBar extends SystemUI implements DemoMode,
             handleCutout(null);
             setForceAmbient();
             updateSettingsTiles();
-            setIconTintOverlay();
         }
     }
 
