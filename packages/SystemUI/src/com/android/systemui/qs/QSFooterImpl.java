22c76a27a9765e12d90511c7964a38065487a87a
Simrat Singh <simrats169169@gmail.com>
2019-12-23 19:09:11 +0530

    QsFooterImpl: Stop the bamboozle
    Signed-off-by: Simrat Singh <simrats169169@gmail.com>
    Change-Id: I26e27a6b016debb55480d73dac7ef3a1234addde

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -314,7 +314,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         final boolean isDemo = UserManager.isDeviceInDemoMode(mContext);
         mMultiUserSwitch.setVisibility(showUserSwitcher() ? View.VISIBLE : View.INVISIBLE);
         mEditContainer.setVisibility(isDemo || !mExpanded ? View.INVISIBLE : View.VISIBLE);
-        mSettingsButton.setVisibility(isDemo || !mExpanded ? View.VISIBLE : View.VISIBLE);
+        mSettingsButton.setVisibility(View.VISIBLE);
     }
 
     private boolean showUserSwitcher() {
