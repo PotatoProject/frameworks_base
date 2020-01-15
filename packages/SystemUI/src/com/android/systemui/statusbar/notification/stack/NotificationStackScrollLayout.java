7e9931a89adfe84244475b8547222c44efbbb565
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-01-15 18:09:48 -0500

    Themes: ensure some elements are fine and dandy after overlay
    - Changes and various inits
    
    Change-Id: I57ff9179091522c14ffcf9a3eecf7492d1fbba9e

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -681,6 +681,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
             mCornerRadius = newRadius;
             invalidate();
         }
+        onUiModeChanged();
         reinflateViews();
     }
 
