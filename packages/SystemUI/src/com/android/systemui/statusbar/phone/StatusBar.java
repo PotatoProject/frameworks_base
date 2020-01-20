b2fcfb860bc2e5fdc1535f588c5a26493a6413b5
abdoi94 <abdoi94.iq@gmail.com>
2020-01-20 17:36:20 +0300

    base: revert all fod-extras from Omni
    Revert "FOD: change a few things about FOD behaviour"
    
    This reverts commit cab3df00cbad71314938e3ede945c221164362a5.
    
    Revert "FOD: let displaymanager handle brightness"
    
    This reverts commit c45c23b9362d8b0be2fc963f272501d5d6465cf9.
    
    Revert "SystemUI: add pulsing callback to KeyguardUpdateMonitor"
    
    This reverts commit bf5d187ed6424f07cbcf70dbdfe7878dba5658da.
    
    Change-Id: I4e1323d234b972498ea8a1bd9a09282353eadaf5
    Signed-off-by: abdoi94 <abdoi94.iq@gmail.com>
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4411,7 +4411,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                     callback.onPulseStarted();
                     updateNotificationPanelTouchState();
                     setPulsing(true);
-                    KeyguardUpdateMonitor.getInstance(mContext).setPulsing(true);
                 }
 
                 @Override
@@ -4424,7 +4423,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                         mStatusBarWindow.suppressWakeUpGesture(false);
                     }
                     setPulsing(false);
-                    KeyguardUpdateMonitor.getInstance(mContext).setPulsing(false);
                 }
 
                 private void setPulsing(boolean pulsing) {
