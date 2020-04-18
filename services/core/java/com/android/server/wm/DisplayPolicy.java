ArrowOS/android_frameworks_base

Navbar: Add navigation bar visibility control [1/2]
Change-Id: Ifa324726d857c5a9351f978ef03480900e468be0

services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -163,6 +163,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.policy.ScreenDecorationsUtils;
 import com.android.internal.util.ScreenShapeHelper;
 import com.android.internal.util.ScreenshotHelper;
+import com.android.internal.util.arrow.ArrowUtils;
 import com.android.internal.util.function.TriConsumer;
 import com.android.internal.widget.PointerLocationView;
 import com.android.server.LocalServices;
@@ -589,16 +590,7 @@ public class DisplayPolicy {
 
         if (mDisplayContent.isDefaultDisplay) {
             mHasStatusBar = true;
-            mHasNavigationBar = mContext.getResources().getBoolean(R.bool.config_showNavigationBar);
-
-            // Allow a system property to override this. Used by the emulator.
-            // See also hasNavigationBar().
-            String navBarOverride = SystemProperties.get("qemu.hw.mainkeys");
-            if ("1".equals(navBarOverride)) {
-                mHasNavigationBar = false;
-            } else if ("0".equals(navBarOverride)) {
-                mHasNavigationBar = true;
-            }
+            mHasNavigationBar = ArrowUtils.deviceSupportNavigationBar(mContext);
         } else {
             mHasStatusBar = false;
             mHasNavigationBar = mDisplayContent.supportsSystemDecorations();
@@ -672,6 +664,15 @@ public class DisplayPolicy {
 
     public boolean hasNavigationBar() {
         return mHasNavigationBar;
+    }
+
+    /**
+     * @hide
+     */
+    public void updatehasNavigationBar() {
+        if (mDisplayContent.isDefaultDisplay) {
+            mHasNavigationBar = ArrowUtils.deviceSupportNavigationBar(mContext);
+        }
     }
 
     public boolean hasStatusBar() {

PotatoProject/frameworks_base

Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>
Change-Id: I6cba5bc0224fd5fe4063e0e9c0e4107eaf94f339

services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -164,6 +164,7 @@ import com.android.internal.policy.ScreenDecorationsUtils;
 import com.android.internal.util.ScreenShapeHelper;
 import com.android.internal.util.ScreenshotHelper;
 import com.android.internal.util.function.TriConsumer;
+import com.android.internal.util.potato.PotatoUtils;
 import com.android.internal.widget.PointerLocationView;
 import com.android.server.LocalServices;
 import com.android.server.UiThread;
@@ -589,16 +590,7 @@ public class DisplayPolicy {
 
         if (mDisplayContent.isDefaultDisplay) {
             mHasStatusBar = true;
-            mHasNavigationBar = mContext.getResources().getBoolean(R.bool.config_showNavigationBar);
-
-            // Allow a system property to override this. Used by the emulator.
-            // See also hasNavigationBar().
-            String navBarOverride = SystemProperties.get("qemu.hw.mainkeys");
-            if ("1".equals(navBarOverride)) {
-                mHasNavigationBar = false;
-            } else if ("0".equals(navBarOverride)) {
-                mHasNavigationBar = true;
-            }
+            mHasNavigationBar = PotatoUtils.deviceSupportNavigationBar(mContext);
         } else {
             mHasStatusBar = false;
             mHasNavigationBar = mDisplayContent.supportsSystemDecorations();
@@ -672,6 +664,15 @@ public class DisplayPolicy {
 
     public boolean hasNavigationBar() {
         return mHasNavigationBar;
+    }
+
+    /**
+     * @hide
+     */
+    public void updatehasNavigationBar() {
+        if (mDisplayContent.isDefaultDisplay) {
+            mHasNavigationBar = PotatoUtils.deviceSupportNavigationBar(mContext);
+        }
     }
 
     public boolean hasStatusBar() {