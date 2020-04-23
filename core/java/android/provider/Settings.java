2c35462becf72fa4f7e0423cdfb5b450e88368d5
rajadeja <rajsjadeja4@gmail.com>
2020-04-23 16:52:02 +0000

    Revert battery spaghet
    Revert "Text battery style: show a bolt ⚡ when charging"
    
    This reverts commit 00d4e50c6c4fb92497e65e0a7163fbf65aa9169d.
    
    Revert "base: Add Circle battery style from PA [1/2]"
    
    This reverts commit 8b7ae75faa5f2c3de059736e1d35c00a85bed370.
    
    Revert "base: Battery styles: Cleanup and fixes"
    
    This reverts commit 69d7cbc05fdd25ec24dca283c9b41de27b1e7476.
    
    Revert "Add battery styles and customizations for Android 10 [1/2]"
    
    This reverts commit 2a5f908ff1d3ab219c24747f4399261026b07ec1.
    
    Change-Id: I14e23a9b5c1e38d50768059a40c468095ccb4fc4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4488,7 +4488,10 @@ public final class Settings {
          *    1 - Show percentage
          * @hide
          */
-        public static final String SHOW_BATTERY_PERCENT = "dummy_show_battery_percent";
+        public static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+        /** @hide */
+        private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
          * Check the proximity sensor during wakeup
@@ -4804,21 +4807,6 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
-         * Battery style
-         * @hide
-         */
-        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
-
-        /**
-          * Statusbar Battery %
-          * 0: Hide the battery percentage
-          * 1: Display the battery percentage
-          * 2: Display the battery percentage only while charging
-          * @hide
-          */
-        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-
-        /**
          * Three Finger Gesture from Oppo
          * @hide
          */
@@ -4882,6 +4870,7 @@ public final class Settings {
             LOCK_TO_APP_ENABLED,
             NOTIFICATION_SOUND,
             ACCELEROMETER_ROTATION,
+            SHOW_BATTERY_PERCENT,
             NOTIFICATION_VIBRATION_INTENSITY,
             RING_VIBRATION_INTENSITY,
             HAPTIC_FEEDBACK_INTENSITY,
@@ -5021,6 +5010,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(POINTER_SPEED);
             PRIVATE_SETTINGS.add(LOCK_TO_APP_ENABLED);
             PRIVATE_SETTINGS.add(EGG_MODE);
+            PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(CUSTOM_BUTTON_EXTRA_KEY_MAPPING);
             PRIVATE_SETTINGS.add(CUSTOM_DEVICE_PROXI_CHECK_ENABLED);
@@ -5131,6 +5121,7 @@ public final class Settings {
             VALIDATORS.put(WIFI_STATIC_NETMASK, WIFI_STATIC_NETMASK_VALIDATOR);
             VALIDATORS.put(WIFI_STATIC_DNS1, WIFI_STATIC_DNS1_VALIDATOR);
             VALIDATORS.put(WIFI_STATIC_DNS2, WIFI_STATIC_DNS2_VALIDATOR);
+            VALIDATORS.put(SHOW_BATTERY_PERCENT, SHOW_BATTERY_PERCENT_VALIDATOR);
             VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
             VALIDATORS.put(CUSTOM_BUTTON_EXTRA_KEY_MAPPING, CUSTOM_BUTTON_EXTRA_KEY_MAPPING_VALIDATOR);
             VALIDATORS.put(CUSTOM_DEVICE_PROXI_CHECK_ENABLED, CUSTOM_DEVICE_PROXI_CHECK_ENABLED_VALIDATOR);
