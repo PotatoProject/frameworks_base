509547fb977a075d67dba62c2f3b38bf4283ccd4
Raj Jadeja <rajsjadeja4@gmail.com>
2020-06-15 05:57:16 +0000

    NavBarActivity: Initialize Dependency when Dagger starts
    same as https://github.com/PotatoProject/frameworks_base/commit/23c220f8e9182d526ff8c6831b23c2f5fe316778
    
    Change-Id: Iaefc42b20204a7a5f379b53325c59f4193ac7096

--- packages/SystemUI/src/com/android/systemui/tuner/NavBarActivity.java
+++ packages/SystemUI/src/com/android/systemui/tuner/NavBarActivity.java
@@ -16,9 +16,10 @@
 package com.android.systemui.tuner;
 
 import android.os.Bundle;
-import android.support.v14.preference.PreferenceFragment;
+import androidx.preference.PreferenceFragment;
 import com.android.systemui.Dependency;
 import com.android.systemui.tuner.TunerActivity;
+import com.android.systemui.SystemUIFactory;
 import com.android.systemui.R;
 
 public class NavBarActivity extends TunerActivity {
@@ -26,7 +27,7 @@ public class NavBarActivity extends TunerActivity {
     private static final String TAG_TUNER = "tuner";
 
     protected void onCreate(Bundle savedInstanceState) {
-        Dependency.initDependencies(this);
+        Dependency.initDependencies(SystemUIFactory.getInstance().getRootComponent());
         super.onCreate(savedInstanceState);
 
         if (getFragmentManager().findFragmentByTag(TAG_TUNER) == null) {
