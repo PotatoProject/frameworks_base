0fec7b87753a73a0f639fa3ab4985a0e0aad5edc
Kshitij Gupta <kshitijgm@gmail.com>
2020-07-07 15:56:11 +0530

    base: notch-city: Add 3 mode display cutout handler
    [@AgentFabulous - POSP]
    
    * About the port
    - This includes the latest patches applied to A10, combined to a single
      change. This includes quite a few changes authored by me, and a ton
      has been heavily changed and rewritten, so I won't even bother listing
      them here.
    
    * The change
    Modes:
     - Normal
     - Immerse
     - Hide
    
    Normal:
      You know this one ¯\_(ツ)_/¯
    
    Immerse:
      This change does not force layout parameters globally. Instead, sets
      statusbar colors and changes layout parameters for statusbar. It also
      forces corners to be drawn with LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER so
      they are always below the statusbar. This however looks weird in
      landscape, thus the feature disables itself in landscape mode. This
      way, we can black out our cutout while being able to see our status
      icons still and have adjustable virtual corners, while not breaking
      any apps.
    
    Hide:
      We can't really register a content observer for overriding the
      resource bool in our java code (causes SystemUI to FC with a null
      pointer ref), so use overlays here. Works like most other "Hide"
    
    Change-Id: I2aa5e08b2f8c5b561873069354063633a2a9f1cc

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -19,12 +19,18 @@ package com.android.systemui.statusbar.phone;
 import static com.android.systemui.ScreenDecorations.DisplayCutoutView.boundsFromDirection;
 
 import android.annotation.ColorInt;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Pair;
 import android.util.TypedValue;
@@ -97,6 +103,10 @@ public class KeyguardStatusBarView extends RelativeLayout
     private ViewGroup mStatusIconArea;
     private int mLayoutState = LAYOUT_NONE;
 
+    private boolean mImmerseMode;
+    private Handler mHandler = new Handler();
+    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+
     /**
      * Draw this many pixels into the left/right side of the cutout to optimally use the space
      */
@@ -126,6 +136,8 @@ public class KeyguardStatusBarView extends RelativeLayout
         loadDimens();
         updateUserSwitcher();
         mBatteryController = Dependency.get(BatteryController.class);
+        updateCutout();
+        mCustomSettingsObserver.observe();
     }
 
     @Override
@@ -167,15 +179,20 @@ public class KeyguardStatusBarView extends RelativeLayout
         lp.setMarginStart(marginStart);
 
         mCarrierLabel.setLayoutParams(lp);
-        updateKeyguardStatusBarHeight();
+        updateCutout();
     }
 
     private void updateKeyguardStatusBarHeight() {
         final int waterfallTop =
                 mDisplayCutout == null ? 0 : mDisplayCutout.getWaterfallInsets().top;
         MarginLayoutParams lp =  (MarginLayoutParams) getLayoutParams();
-        lp.height =  getResources().getDimensionPixelSize(
-                R.dimen.status_bar_header_height_keyguard) + waterfallTop;
+        if (mImmerseMode) {
+            lp.height =  getResources().getDimensionPixelSize(
+                    R.dimen.status_bar_height);
+        } else {
+            lp.height =  getResources().getDimensionPixelSize(
+                    R.dimen.status_bar_header_height_keyguard) + waterfallTop;
+        }
         setLayoutParams(lp);
     }
 
@@ -530,5 +547,35 @@ public class KeyguardStatusBarView extends RelativeLayout
         if (mBatteryView != null) {
             mBatteryView.dump(fd, pw, args);
         }
+    }
+
+    private class CustomSettingsObserver extends ContentObserver {
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DISPLAY_CUTOUT_MODE), false, this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(Settings.System.DISPLAY_CUTOUT_MODE))) {
+                updateCutout();
+            }
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateCutout();
+        }
+    }
+
+    private void updateCutout() {
+        mImmerseMode = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.DISPLAY_CUTOUT_MODE, 0, UserHandle.USER_CURRENT) == 1;
+        updateKeyguardStatusBarHeight();
     }
 }
