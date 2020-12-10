314539677f486821ec2b8fc28987c8e5b96d3f53
calebcabob <calphonic@gmail.com>
2020-12-10 15:19:04 -0500

    Allow enabling/disabling Android 11 powermenu by ezio84 [1/2]
    Something similar to this can be done for customizing
    the power menu as well (add/remove items).
    
    Change-Id: I9b58abcef9d88fbec6592fa31760c5d9d6ebeebc
    Signed-off-by: calebcabob <calphonic@gmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4911,6 +4911,11 @@ public final class Settings {
         public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
         /**
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
