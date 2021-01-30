ac63b0520dae4ca1305b8bd721228e1ae9417ab1
SahilSonar <sss.sonar2003@gmail.com>
2021-01-30 07:16:27 +0100

    base: allow for customizing the length of the navigation handle [1/2]
    [@SahilSonar - POSP]
    - Forward ported to Android 11
    
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Change-Id: Icbeddbf8c76d743f06abc872511694dfee676a35

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5027,6 +5027,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SYSTEMUI_PLUGIN_VOLUME);
             PRIVATE_SETTINGS.add(DISPLAY_CUTOUT_MODE);
             PRIVATE_SETTINGS.add(STOCK_STATUSBAR_IN_HIDE);
+            PRIVATE_SETTINGS.add(NAVIGATION_HANDLE_WIDTH);
         }
 
         /**
@@ -9115,13 +9116,13 @@ public final class Settings {
          * @hide
          */
         public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
- 
+
         /**
          * Boolean value whether to link ringtone and notification volume
          * @hide
          */
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
-        
+
         /**
          * Whether applications can fake a signature.
          * 1 = permit apps to fake signature
@@ -9129,6 +9130,11 @@ public final class Settings {
          * @hide
          */
         public static final String ALLOW_SIGNATURE_FAKE = "allow_signature_fake";
+
+        /** Width of the navigation handle.
+         *  @hide
+         */
+        public static final String NAVIGATION_HANDLE_WIDTH = "navigation_handle_width";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
