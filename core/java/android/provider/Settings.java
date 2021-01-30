f76603ae52348930f8926e3a9cfc311a380f8f0b
anayw2001 <awadhera@berkeley.edu>
2021-01-30 07:16:27 +0100

    base: allow for customizing the length of the navigation handle [1/2]
    [@HrX03 - POSP]
    - Normally, we would handle this using overlays, but at the cost of limited flexibility and control. Additionally, often using overlays on certain Quickstep components renders them unresponsive on A11 for some odd reason.
    - Add in settings to control the length of the navigation pill, and control this from Fries directly.
    
    [@SahilSonar - POSP]
    - Forward ported to Android 12
    
    Co-authored-by: HrX03 <dn.bianco03@gmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Change-Id: Icbeddbf8c76d743f06abc872511694dfee676a35

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5164,6 +5164,11 @@ public final class Settings {
          * the setting value. See an example above.
          */
 
+        /** Width of the navigation handle.
+         *  @hide
+         */
+        public static final String NAVIGATION_HANDLE_WIDTH = "navigation_handle_width";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -5284,6 +5289,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE_VENDOR_HINT);
+            PRIVATE_SETTINGS.add(NAVIGATION_HANDLE_WIDTH);
         }
 
         /**
