27aebb0981666122c72853b1504edc68a030227b
Kshitij Gupta <kshitijgm@gmail.com>
2021-02-27 10:46:06 +0100

    base: Bring back accent disco!
    Change-Id: I6ab5c65d12e337476134f6a256cdf214c17cf347

--- core/java/android/content/res/AccentUtils.java
+++ core/java/android/content/res/AccentUtils.java
@@ -1,12 +1,13 @@
 package android.content.res;
 
 import android.annotation.Nullable;
+import android.app.ActivityThread;
+import android.content.Context;
+import android.content.res.ColorUtils;
 import android.graphics.Color;
 import android.os.SystemProperties;
-import android.util.Log;
 import android.provider.Settings;
-import android.content.Context;
-import android.app.ActivityThread;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,6 +19,7 @@ public final class AccentUtils {
 
     private static final String ACCENT_DARK_SETTING = "accent_dark";
     private static final String ACCENT_LIGHT_SETTING = "accent_light";
+    private static final String ACCENT_DISCO_SETTING = "accent_disco";
 
     public static boolean isResourceDarkAccent(@Nullable String resName) {
         return resName == null
@@ -42,6 +44,9 @@ public final class AccentUtils {
     private static int getAccentColor(int defaultColor, String setting) {
         final Context context = ActivityThread.currentApplication();
         try {
+            if (Settings.Secure.getInt(context.getContentResolver(), ACCENT_DISCO_SETTING, 0) == 1) {
+                return ColorUtils.genRandomAccentColor(setting == ACCENT_DARK_SETTING);
+            }
             String colorValue = Settings.Secure.getString(context.getContentResolver(), setting);
             return (colorValue == null || "-1".equals(colorValue))
                     ? defaultColor
