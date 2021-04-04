58c0a4a3ecaa4c2de328e75059c43c2de589b05b
Kshitij Gupta <kshitijgm@gmail.com>
2021-04-05 02:50:19 +0530

    QSPanel: Wait before attempting to get wallpaper colors
    Change-Id: Iaa9671440f35e45364c8f2b8166f67a65d163525

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1863,6 +1863,10 @@ public class StatusBar extends SystemUI implements DemoMode,
     @Override
     public void onColorsChanged(ColorExtractor extractor, int which) {
         updateTheme();
+        mHandler.postDelayed(() -> {
+            mQSPanel.getHost().reloadAllTiles();
+        },
+        1000);
     }
 
     @Nullable
