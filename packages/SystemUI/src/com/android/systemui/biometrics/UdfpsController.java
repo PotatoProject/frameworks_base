1fb7ed0f484168b3777d1ca306a8e6f53d8c967e
TheScarastic <warabhishek@gmail.com>
2021-10-06 06:53:09 +0000

    udfps: Add support for udfps on aod without having dedicated sensor
    * make it overlayable while at it
    
    Change-Id: I4aa39efe3714bcc40ab3da862809ca9c8974c6bd
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -165,6 +165,7 @@ public class UdfpsController implements DozeReceiver {
     private boolean mOnFingerDown;
     private boolean mAttemptedToDismissKeyguard;
     private Set<Callback> mCallbacks = new HashSet<>();
+    private final int mUdfpsVendorCode;
 
     @VisibleForTesting
     public static final AudioAttributes VIBRATION_SONIFICATION_ATTRIBUTES =
@@ -318,6 +319,19 @@ public class UdfpsController implements DozeReceiver {
                     return;
                 }
                 mView.setDebugMessage(message);
+            });
+        }
+
+        @Override
+        public void onAcquired(int sensorId, int acquiredInfo, int vendorCode) {
+            mFgExecutor.execute(() -> {
+                if (acquiredInfo == 6 && (mStatusBarStateController.isDozing() || !mScreenOn)) {
+                    if (vendorCode == mUdfpsVendorCode) {
+                        mPowerManager.wakeUp(mSystemClock.uptimeMillis(),
+                                PowerManager.WAKE_REASON_GESTURE, TAG);
+                        onAodInterrupt(0, 0, 0, 0); // To-Do pass proper values
+                    }
+                }
             });
         }
     }
@@ -645,6 +659,9 @@ public class UdfpsController implements DozeReceiver {
         context.registerReceiver(mBroadcastReceiver, filter);
 
         udfpsHapticsSimulator.setUdfpsController(this);
+
+        mUdfpsVendorCode = mContext.getResources().getInteger(R.integer.config_udfps_vendor_code);
+
     }
 
     /**
