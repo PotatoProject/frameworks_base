b36aa0a4878eda30ad9dc73d2b630740bc0fe933
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: Integrate smartspace and backgesture provider
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I5ed27c661309f3de333c7d42e0af4cead499116e

--- packages/SystemUI/src/com/posp/android/systemui/SystemUIPospFactory.java
+++ packages/SystemUI/src/com/posp/android/systemui/SystemUIPospFactory.java
@@ -1,12 +1,23 @@
 package com.posp.android.systemui;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Handler;
+
+import com.google.android.systemui.gesture.BackGestureTfClassifierProviderGoogle;
 
 import com.posp.android.systemui.dagger.DaggerGlobalRootComponentPosp;
 import com.posp.android.systemui.dagger.GlobalRootComponentPosp;
+import com.posp.android.systemui.dagger.SysUIComponentPosp;
+
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
+import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
+import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class SystemUIPospFactory extends SystemUIFactory {
     @Override
@@ -14,5 +25,18 @@ public class SystemUIPospFactory extends SystemUIFactory {
         return DaggerGlobalRootComponentPosp.builder()
                 .context(context)
                 .build();
+    }
+
+    @Override
+    public BackGestureTfClassifierProvider createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
+        return new BackGestureTfClassifierProviderGoogle(am, modelName);
+    }
+
+    @Override
+    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
+        super.init(context, fromTest);
+        if (shouldInitializeComponents()) {
+            ((SysUIComponentPosp) getSysUIComponent()).createKeyguardSmartspaceController();
+        }
     }
 }
--- packages/SystemUI/src/com/posp/android/systemui/dagger/SysUIComponentPosp.java
+++ packages/SystemUI/src/com/posp/android/systemui/dagger/SysUIComponentPosp.java
@@ -7,6 +7,9 @@ import com.android.systemui.dagger.SystemUIBinder;
 import com.android.systemui.dagger.SysUIComponent;
 import com.android.systemui.dagger.SystemUIModule;
 
+import com.posp.android.systemui.keyguard.KeyguardSliceProviderPosp;
+import com.posp.android.systemui.smartspace.KeyguardSmartspaceController;
+
 import dagger.Subcomponent;
 
 @SysUISingleton
@@ -22,4 +25,12 @@ public interface SysUIComponentPosp extends SysUIComponent {
     interface Builder extends SysUIComponent.Builder {
         SysUIComponentPosp build();
     }
+
+    /**
+     * Member injection into the supplied argument.
+     */
+    void inject(KeyguardSliceProviderPosp keyguardSliceProviderPosp);
+
+    @SysUISingleton
+    KeyguardSmartspaceController createKeyguardSmartspaceController();
 }
