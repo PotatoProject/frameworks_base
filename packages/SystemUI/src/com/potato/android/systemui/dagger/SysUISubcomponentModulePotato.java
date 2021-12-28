8541941c3d749d68ee50234415eefad281dc60a8
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    SystemUI: Initial SystemUI stub
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I14c37bd35b3878d1e4abae8ce33bf93f11a32a6d

---
+++ packages/SystemUI/src/com/potato/android/systemui/dagger/SysUISubcomponentModulePotato.java
@@ -0,0 +1,10 @@
+package com.potato.android.systemui.dagger;
+
+import dagger.Module;
+
+/**
+ * Dagger module for including the WMComponent.
+ */
+@Module(subcomponents = {SysUIComponentPotato.class})
+public abstract class SysUISubcomponentModulePotato {
+}
