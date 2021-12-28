802fb9e51988863048e46f950996e16a7a55274f
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: Integrate smartspace and backgesture provider
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I3453bafeee6899b06c8529a0c1af2d0b08f87afc

--- packages/SystemUI/src/com/potato/android/systemui/SystemUIPotatoFactory.java
+++ packages/SystemUI/src/com/potato/android/systemui/SystemUIPotatoFactory.java
@@ -1,12 +1,22 @@
 package com.potato.android.systemui;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Handler;
+
+import com.google.android.systemui.gesture.BackGestureTfClassifierProviderGoogle;
 
 import com.potato.android.systemui.dagger.DaggerGlobalRootComponentPotato;
 import com.potato.android.systemui.dagger.GlobalRootComponentPotato;
+import com.potato.android.systemui.dagger.SysUIComponentPotato;
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
+import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
+import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class SystemUIPotatoFactory extends SystemUIFactory {
     @Override
@@ -14,5 +24,18 @@ public class SystemUIPotatoFactory extends SystemUIFactory {
         return DaggerGlobalRootComponentPotato.builder()
                 .context(context)
                 .build();
+    }
+
+    @Override
+    public BackGestureTfClassifierProvider createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
+        return new BackGestureTfClassifierProviderGoogle(am, modelName);
+    }
+
+    @Override
+    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
+        super.init(context, fromTest);
+        if (shouldInitializeComponents()) {
+            ((SysUIComponentYaap) getSysUIComponent()).createKeyguardSmartspaceController();
+        }
     }
 }
--- packages/SystemUI/src/com/potato/android/systemui/dagger/SysUIComponentPotato.java
+++ packages/SystemUI/src/com/potato/android/systemui/dagger/SysUIComponentPotato.java
@@ -7,6 +7,9 @@ import com.android.systemui.dagger.SystemUIBinder;
 import com.android.systemui.dagger.SysUIComponent;
 import com.android.systemui.dagger.SystemUIModule;
 
+import com.potato.android.systemui.keyguard.KeyguardSliceProviderPotato;
+import com.potato.android.systemui.smartspace.KeyguardSmartspaceController;
+
 import dagger.Subcomponent;
 
 @SysUISingleton
@@ -22,4 +25,12 @@ public interface SysUIComponentPotato extends SysUIComponent {
     interface Builder extends SysUIComponent.Builder {
         SysUIComponentPotato build();
     }
+
+    /**
+     * Member injection into the supplied argument.
+     */
+    void inject(KeyguardSliceProviderPotato keyguardSliceProviderPotato);
+
+    @SysUISingleton
+    KeyguardSmartspaceController createKeyguardSmartspaceController();
 }
