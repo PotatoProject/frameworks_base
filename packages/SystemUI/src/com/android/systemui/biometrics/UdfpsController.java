9ea6cf9d3037b83e97159b8e7b691f3c3323ab3c
LuK1337 <priv.luk@gmail.com>
2022-02-21 23:33:58 +0100

    udfps: Implement default udfps hbm provider
    This should let us choose between multiple UdfpsHbmProvider
    implementations.
    
    Change-Id: I9b93e32644feaf1398cdac69e9696d8ec195f246
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -591,7 +591,7 @@ public class UdfpsController implements DozeReceiver {
             @NonNull ScreenLifecycle screenLifecycle,
             @Nullable Vibrator vibrator,
             @NonNull UdfpsHapticsSimulator udfpsHapticsSimulator,
-            @NonNull Optional<UdfpsHbmProvider> hbmProvider,
+            @NonNull UdfpsHbmProvider hbmProvider,
             @NonNull KeyguardStateController keyguardStateController,
             @NonNull KeyguardBypassController keyguardBypassController,
             @NonNull DisplayManager displayManager,
@@ -620,7 +620,7 @@ public class UdfpsController implements DozeReceiver {
         mPowerManager = powerManager;
         mAccessibilityManager = accessibilityManager;
         mLockscreenShadeTransitionController = lockscreenShadeTransitionController;
-        mHbmProvider = hbmProvider.orElse(null);
+        mHbmProvider = hbmProvider;
         screenLifecycle.addObserver(mScreenObserver);
         mScreenOn = screenLifecycle.getScreenState() == ScreenLifecycle.SCREEN_ON;
         mKeyguardBypassController = keyguardBypassController;
