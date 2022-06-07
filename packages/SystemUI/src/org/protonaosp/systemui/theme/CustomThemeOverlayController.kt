0c5524f19579819312ba6c69879889a3e8e44656
SahilSonar <sss.sonar2003@gmail.com>
2022-06-07 11:36:43 +0200

    [DNM] Revert "SystemUI: Integrate smartspace and backgesture provider"
    Change-Id: I31dfdd5b7205349019fc2421be7efb44dc00e4e8

--- packages/SystemUI/src/org/protonaosp/systemui/theme/CustomThemeOverlayController.kt
+++ packages/SystemUI/src/org/protonaosp/systemui/theme/CustomThemeOverlayController.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.posp.android.systemui.theme
+package org.protonaosp.systemui.theme
 
 import android.annotation.ColorInt
 import android.app.WallpaperColors
@@ -56,7 +56,7 @@ import kotlin.math.log10
 import kotlin.math.pow
 
 @SysUISingleton
-class ThemeOverlayControllerPosp @Inject constructor(
+class CustomThemeOverlayController @Inject constructor(
     private val context: Context,
     broadcastDispatcher: BroadcastDispatcher,
     @Background bgHandler: Handler,
@@ -219,7 +219,7 @@ class ThemeOverlayControllerPosp @Inject constructor(
     }
 
     companion object {
-        private const val TAG = "ThemeOverlayControllerPosp"
+        private const val TAG = "CustomThemeOverlayController"
 
         private const val PREF_PREFIX = "monet_engine"
         private const val PREF_CUSTOM_COLOR = "${PREF_PREFIX}_custom_color"
